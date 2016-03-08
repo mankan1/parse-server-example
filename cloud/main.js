b56a925e70750ac3ea82a7377d8c869e49ba16b0
Manoj Kandlikar <manojkandlikar@Manojs-MBP-2.T-mobile.com>
2016-03-07 22:22:51 -0800

    initial commit

--- cloud/main.js
+++ cloud/main.js
@@ -1,4 +1,241 @@
+Parse.Cloud.define("sendAnnouncement", function(request, response) {
+        var name = request.params.senderName;
+        var msg = request.params.message;
+
+        Parse.Push.send({
+                channels: [ request.params.accountId ],
+                data: {
+                        title: name,
+                        message: msg,
+                        action: "com.hello.announcement.sample.SEND_ANNOUNCEMENT",
+                        senderId: request.params.senderId,
+                        accountId: request.params.accountId
+                }
+        }, {
+                success: function() {
+                        // Push was successful
+                        response.success("sendAnnouncement sent");
+                },
+                error: function(error) {
+                        // Handle error
+                        response.error("error with sendAnnouncement: " + error);
+                },
+                useMasterKey: true
+        });
+});
+
+Parse.Cloud.define("driverCancelledNotif", function(request, response) {
+        var msg = request.params.msg;
+        var passengerNumber = request.params.passengerNumber;
+
+        Parse.Push.send({
+                channels: [ request.params.channel ],
+                data: {
+                        passengerNumber: passengerNumber,
+                        msg : msg,
+                        action: "onetaxicabs.DRIVERCANCELLEDNOTIF_ANNOUNCEMENT"
+                }
+        }, {
+                success: function() {
+                        // Push was successful
+                        response.success("driverCancelledNotif sendAnnouncement sent");
+                },
+                error: function(error) {
+                        // Handle error
+                        response.error("error with sendAnnouncement: " + error);
+                },
+                useMasterKey: true
+        });
+});
+
+
+Parse.Cloud.define("declineTodriverNotif", function(request, response) {
+        var msg = request.params.msg;
+        var phone = request.params.phone;
+
+        Parse.Push.send({
+                channels: [ request.params.channel ],
+                data: {
+                        phone: phone,
+                        msg : msg,
+                        action: "onetaxicabs.DECLINETODRIVERNOTIF_ANNOUNCEMENT"
+                }
+        }, {
+                success: function() {
+                        // Push was successful
+                        response.success("driverCancelledNotif sendAnnouncement sent");
+                },
+                error: function(error) {
+                        // Handle error
+                        response.error("error with sendAnnouncement: " + error);
+                },
+                useMasterKey: true
+        });
+});
+
+
+Parse.Cloud.define("passengerCancelledTodriverNotif", function(request, response) {
+        var msg = request.params.msg;
+        var phone = request.params.phone;
+
+        Parse.Push.send({
+                channels: [ request.params.channel ],
+                data: {
+                        phone: phone,
+                        msg : msg,
+                        action: "onetaxicabs.PASSENGERCANCELLEDTODRIVERNOTIF_ANNOUNCEMENT"
+                }
+        }, {
+                success: function() {
+                        // Push was successful
+                        response.success("driverCancelledNotif sendAnnouncement sent");
+                },
+                error: function(error) {
+                        // Handle error
+                        response.error("error with sendAnnouncement: " + error);
+                },
+                useMasterKey: true
+        });
+});
+
+Parse.Cloud.define("acceptDeclineNotif", function(request, response) {
+        var msg = request.params.msg;
+        var phone = request.params.phone;
+        var username = request.params.username;
+        var latitude = request.params.latitude;
+        var longitude = request.params.longitude;
+        var passlatitude = request.params.passlatitude;
+        var passlongitude = request.params.passlongitude;
+        var farerecepientphonenumbers = request.params.farerecepientphonenumbers;
+        var farerecepientsenderids = request.params.farerecepientsenderids;
+
+        Parse.Push.send({
+                channels: [ request.params.channel ],
+                data: {
+        		phone: phone,
+        		username : username,
+                        latitude: latitude,
+                        longitude: longitude,
+                        passlatitude: passlatitude,
+                        passlongitude: passlongitude,
+                        msg : msg,
+                        action: "onetaxicabs.ACCEPTDECLINENOTIF_ANNOUNCEMENT",
+        		farerecepientphonenumbers : farerecepientphonenumbers,
+        		farerecepientsenderids : farerecepientsenderids
+                }
+        }, {
+                success: function() {
+                        // Push was successful
+                        response.success("sendAnnouncement sent");
+                },
+                error: function(error) {
+                        // Handle error
+                        response.error("error with sendAnnouncement: " + error);
+                },
+                useMasterKey: true
+        });
+});
+
+
+Parse.Cloud.define("acceptanceToDriverNotif", function(request, response) {
+        var msg = request.params.msg;
+        var phone = request.params.phone;
+        var passengernumber = request.params.passengernumber;
+        var latitude = request.params.latitude;
+        var longitude = request.params.longitude;
+        var passlatitude = request.params.passlatitude;
+        var passlongitude = request.params.passlongitude;
+        var distance = request.params.distance;
+        var username = request.params.username;
+
+        Parse.Push.send({
+                channels: [ request.params.channel ],
+                data: {
+                        phone: phone,
+                        username : username,
+                        latitude: latitude,
+                        longitude: longitude,
+                        passlatitude: passlatitude,
+                        passlongitude: passlongitude,
+                        msg : msg,
+        		passengernumber : passengernumber,
+                        action: "onetaxicabs.ACCEPTANCETODRIVER_ANNOUNCEMENT",
+                        distance : distance
+                }
+        }, {
+                success: function() {
+                        // Push was successful
+                        response.success("sendAnnouncement sent");
+                },
+                error: function(error) {
+                        // Handle error
+                        response.error("error with sendAnnouncement: " + error);
+                },
+                useMasterKey: true
+        });
+});
+
+
+Parse.Cloud.define("acceptanceToPassNotif", function(request, response) {
+        var msg = request.params.msg;
+        var phone = request.params.phone;
+        var passengernumber = request.params.passengernumber;
+        var latitude = request.params.latitude;
+        var longitude = request.params.longitude;
+        var distance = request.params.distance;
+        var username = request.params.username;
+
+        Parse.Push.send({
+                channels: [ request.params.channel ],
+                data: {
+                        phone: phone,
+                        username : username,
+                        latitude: latitude,
+                        longitude: longitude,
+                        msg : msg,
+                        passengernumber : passengernumber,
+                        action: "onetaxicabs.ACCEPTANCETOPASS_ANNOUNCEMENT",
+                        distance : distance
+                }
+        }, {
+                success: function() {
+                        // Push was successful
+                        response.success("sendAnnouncement sent");
+                },
+                error: function(error) {
+                        // Handle error
+                        response.error("error with sendAnnouncement: " + error);
+                },
+                useMasterKey: true
+        });
+});
+
+Parse.Cloud.define("notifyOtherDriversNotif", function(request, response) {
+        var msg = request.params.msg;
+        var phone = request.params.phone;
+	var othersList = request.params.othersList;
+
+        Parse.Push.send({
+                channels: [ request.params.channel ],
+                data: {
+                        phone: phone,
+                        msg : msg,
+			othersList : othersList,
+                        action: "onetaxicabs.OTHERDRIVERSNOTIF_ANNOUNCEMENT"
+                }
+        }, {
+                success: function() {
+                        // Push was successful
+                        response.success("driverCancelledNotif sendAnnouncement sent");
+                },
+                error: function(error) {
+                        // Handle error
+                        response.error("error with sendAnnouncement: " + error);
+                },
+                useMasterKey: true
+        });
+});
 
 Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hello from Azure.');
+  res.success('Hi');
 });
--- index.js
+++ index.js
@@ -13,8 +13,16 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '' //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || '32ypRKDpwMEwsIgEMxjwXYXQ126pF78NkdHpO5fe',
+  masterKey: process.env.MASTER_KEY || 'xcM6bh8evgQX0M4A9SxUBw6zJ7pfZf4Aw35IEK2t',
+  clientKey: 'aRL2Mi1T0unTjtWLK0Uls53jwezxV9A5B6hVgpyH', //Add your master key here. Keep it secret!
+  dotNetKey: 'jGoLsg4Ln56EeTfT8DBg5d1hnKDQAAxxlzCchR0p',
+push: {
+    android: {
+      senderId: '33781838442', // The Sender ID of GCM
+      apiKey: 'AIzaSyCYec5hqYS5vf4kT68jSty10Cc1jKFkdhs' // The Server API Key of GCM
+    }
+  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
